5fa76451172fd78ed4164b0b167e42f6c79b8786
Farman71 <farmanhussain71@gmail.com>
2024-12-31 16:32:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfYear(numdays) {
+	if(numdays%4==0 && numdays%100!=0 || numdays%400==0){
+		return 366;
+	}
+	else{
+		return 365;
+	}
+}
+let numdays = prompt("Enter the Year:");
+alert(daysOfYear(numdays));
